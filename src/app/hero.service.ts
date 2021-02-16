539d6a042268f0dd4244acc2f076b2d662ca9ac3
bbbence <benceborbas88@gmail.com>
2021-02-16 11:57:17 +0100

    Table header works.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  getAllHeroes() {
+    throw new Error('Method not implemented.');
+  }
+
+
+  list: Hero[] = [
+    {
+      id: 1,
+      name: 'Superman',
+      superPower: 'flight',
+      address: 'Metropolis'
+    },
+    {
+      id: 2,
+      name: 'Wonder Woman',
+      superPower: 'strength',
+      address: 'Themyscira'
+    },
+    {
+      id: 3,
+      name: 'Wolverine',
+      superPower: 'claws',
+      address: 'Canada'
+    },
+    {
+      id: 4,
+      name: 'Catwoman',
+      superPower: 'stealth',
+      address: 'Gotham'
+    },
+    {
+      id: 5,
+      name: 'Jean Grey',
+      superPower: 'telekinesis',
+      address: 'New York'
+    },
+    {
+      id: 6,
+      name: 'Thor',
+      superPower: 'thunder',
+      address: 'Asgard'
+    }
+  ];
 
   constructor() { }
 }
